fabdf4f911234c208d7b75fcaa38b312f06ebe2f
v7lin <v7lin@qq.com>
2021-03-16 19:35:25 +0800

    初始化

---
+++ ios/Classes/DeviceKitPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface DeviceKitPlugin : NSObject<FlutterPlugin>
+@end
---
+++ test/device_kit_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  const MethodChannel channel = MethodChannel('device_kit');
+
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() {
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {});
+  });
+
+  tearDown(() {
+    channel.setMockMethodCallHandler(null);
+  });
+}
